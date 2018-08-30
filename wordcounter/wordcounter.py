b340f207f305da48c208297bde2e51c67f7dee23
João M. Campagnolo <jota.campagnolo@gmail.com>
2018-08-30 15:13:16 -0300

    Added news.in and wiki.in, generating two different outs with words amount.

--- wordcounter/wordcounter.py
+++ wordcounter/wordcounter.py
@@ -1,32 +1,51 @@
+import os
+import json
 from operator import itemgetter
 from collections import OrderedDict
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import operator
 
-IN_FILE = open('in.txt','r+')
-text = IN_FILE.read().lower()
+PATH_IN = 'sports_in/'
+PATH_OUT = 'sports_out/'
+IN_FILES = []
 stop_words = set(stopwords.words('english'))
+news_dict = {}
+wiki_dict = {}
 
-clean_text = text
-for char in '!\"#$%&\'()*+,-./:;<=>?@[]\\_0123456789':
-    clean_text = clean_text.replace(char, '')
+for file in os.listdir(PATH_IN):
+    IN_FILES.append(str(file))
 
-text_tokens = word_tokenize(clean_text)
+for file in IN_FILES:
+    IN_FILE = open(PATH_IN+file,'r+')
+    text = IN_FILE.read().lower()
+    clean_text = text
+    for char in '-':
+        clean_text = clean_text.replace(char, ' ')
+    for char in '!\"#$%&\'“”—’()*+,./:;<=>?@[]\\_0123456789':
+        clean_text = clean_text.replace(char, '')
+    text_tokens = word_tokenize(clean_text)
+    clean_tokens = []
+    for token in text_tokens:
+        if token not in stop_words:
+            clean_tokens.append(token)
+    for token in clean_tokens:
+        if file.find('wiki') >= 0:
+            if token not in wiki_dict:
+                wiki_dict[token] = 1
+            else:
+                wiki_dict[token] += 1
+        else:
+            if token not in news_dict:
+                news_dict[token] = 1
+            else:
+                news_dict[token] += 1
 
-clean_tokens = []
-for token in text_tokens:
-    if token not in stop_words:
-        clean_tokens.append(token)
+news_dict = sorted(news_dict.items(), reverse=True, key=operator.itemgetter(1))
+wiki_dict = sorted(wiki_dict.items(), reverse=True, key=operator.itemgetter(1))
 
-word_dict = {}
-for token in clean_tokens:
-    if token not in word_dict:
-        word_dict[token] = 1
-    else:
-        word_dict[token] += 1
+with open(PATH_OUT+'news_out.txt', 'w') as file:
+     file.write(json.dumps(news_dict))
 
-word_dict = sorted(word_dict.items(), reverse=True, key=operator.itemgetter(1))
-
-for word in word_dict:
-    print(word)
+with open(PATH_OUT+'wiki_out.txt', 'w') as file:
+     file.write(json.dumps(wiki_dict))
