75265f02b9a730c0f9c3434d33c6d77922f1fd37
João M. Campagnolo <jota.campagnolo@gmail.com>
2018-08-30 16:57:13 -0300

    words now are stemmized.

--- wordcounter/wordcounter.py
+++ wordcounter/wordcounter.py
@@ -1,15 +1,16 @@
 import os
-import json
 from operator import itemgetter
 from collections import OrderedDict
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import PorterStemmer
 import operator
 
 PATH_IN = 'sports_in/'
 PATH_OUT = 'sports_out/'
 IN_FILES = []
 stop_words = set(stopwords.words('english'))
+STEM_FUNC = PorterStemmer()
 news_dict = {}
 wiki_dict = {}
 
@@ -22,14 +23,18 @@ for file in IN_FILES:
     clean_text = text
     for char in '-':
         clean_text = clean_text.replace(char, ' ')
-    for char in '!\"#$%&\'“”—’()*+,./:;<=>?@[]\\_0123456789':
+    for char in '!\"#$%&\'“”—–’()*+,./:;<=>?@[]\\_0123456789':
         clean_text = clean_text.replace(char, '')
     text_tokens = word_tokenize(clean_text)
     clean_tokens = []
     for token in text_tokens:
         if token not in stop_words:
             clean_tokens.append(token)
+    stem_tokens = []
     for token in clean_tokens:
+        stem_tokens.append(STEM_FUNC.stem(token))
+
+    for token in stem_tokens:
         if file.find('wiki') >= 0:
             if token not in wiki_dict:
                 wiki_dict[token] = 1
@@ -45,7 +50,9 @@ news_dict = sorted(news_dict.items(), reverse=True, key=operator.itemgetter(1))
 wiki_dict = sorted(wiki_dict.items(), reverse=True, key=operator.itemgetter(1))
 
 with open(PATH_OUT+'news_out.txt', 'w') as file:
-     file.write(json.dumps(news_dict))
+    for word in news_dict:
+        file.write(str(word)+'\n')
 
 with open(PATH_OUT+'wiki_out.txt', 'w') as file:
-     file.write(json.dumps(wiki_dict))
+    for word in wiki_dict:
+        file.write(str(word)+'\n')
